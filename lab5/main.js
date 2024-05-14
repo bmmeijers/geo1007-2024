4f1f4d05b0f9678a997e7367f1e41af1543d8957
peoplearesinging <150631996+peoplearesinging@users.noreply.github.com>
2024-05-14 14:11:23 +0200

    recover

--- lab5/main.js
+++ lab5/main.js
@@ -1,11 +1,9 @@
-// First, define the projection with proj4js
-proj4.defs("EPSG:28992", "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +datum=rdnap +units=m +no_defs");
-
-// Define the map with the specific CRS
+// Definition Rijksdriehoekstelsel (EPSG:28992)
 let res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105];
 let map = L.map('map-canvas', {
     continuousWorld: true,
-    crs: new L.Proj.CRS('EPSG:28992', {
+    crs: new L.Proj.CRS('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', {
+        transformation: L.Transformation(-1, -1, 0, 0),
         resolutions: res,
         origin: [-285401.920, 903401.920],
         bounds: L.bounds([-285401.920, 903401.920], [595401.920, 22598.080])
@@ -13,17 +11,17 @@ let map = L.map('map-canvas', {
     layers: [],
     center: [52.010, 4.36744],
     zoom: 9,
-});
 
+});
 map.attributionControl.setPrefix('');
 
 // BRT - (Base Registry Topography) BaseMap PDOK:
-let options = { maxZoom: 14, attribution: 'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>' };
+let options = { maxZoom: 14, attribution: 'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>' }
 let basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:28992/{z}/{x}/{y}.png', options);
 
 basemap_pdok.getAttribution = function () {
     return 'BRT Achtergrondkaart <a href="http://www.kadaster.nl">Kadaster</a>.';
-};
+}
 basemap_pdok.addTo(map);
 
 // To group the base layers (background) and make the ToC widget
@@ -32,8 +30,8 @@ let baseLayers = {
 };
 let toc = L.control.layers(baseLayers).addTo(map);
 
-// Register a geocoder to the map app
-function register_geocoder(mapInstance) {
+// Register a geocoder to the map app 
+register_geocoder = function (mapInstance) {
     let polygon = null;
 
     function clear() {
@@ -46,7 +44,7 @@ function register_geocoder(mapInstance) {
         defaultMarkGeocode: false
     })
         .on('markgeocode', function (e) {
-            clear();
+            clear()
             var bbox = e.geocode.bbox;
             polygon = L.polygon([
                 bbox.getSouthEast(),
@@ -62,36 +60,32 @@ function register_geocoder(mapInstance) {
     return geocoder;
 }
 
-register_geocoder(map);
+register_geocoder(map)
 
 function registerGeoLocate(mapInstance) {
-    // Request the device's location
-    mapInstance.locate({ setView: true, maxZoom: 16, watch: true });
+    mapInstance.locate({ setView: true, maxZoom: 16 });
 
-    // Event handler for when location is found
     function onLocationFound(e) {
-        var radius = e.accuracy / 2; // Get accuracy radius, divide by 2 for diameter
+        var radius = e.accuracy;
 
-        let marker = L.marker(e.latlng).addTo(mapInstance)
+        let m = L.marker(e.latlng).addTo(map)
             .bindPopup("You are within " + radius.toFixed(1) + " meters from this point").openPopup();
 
-        let circle = L.circle(e.latlng, { radius: radius }).addTo(mapInstance);
+        let c = L.circle(e.latlng, radius).addTo(map);
 
-        // Optional: remove location marker and circle after 25 seconds
         setTimeout(function () {
-            mapInstance.removeLayer(marker);
-            mapInstance.removeLayer(circle);
-        }, 25000);
-    }
-
-    // Event handler for location errors (e.g., user denies geolocation)
-    function onLocationError(e) {
-        alert("Location access denied: " + e.message);
+            mapInstance.removeLayer(m);
+            mapInstance.removeLayer(c);
+        },
+            25000);
     }
 
     mapInstance.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
     mapInstance.on('locationerror', onLocationError);
 }
-
-// Initialize the geolocation request when the map is ready
-registerGeoLocate(map);
+registerGeoLocate(map)
